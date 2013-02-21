250e8a5b272979f54d9c3b51e959d7477c9a6cb7
Yecheng Fu <cofyc.jackson@gmail.com>
2013-02-21 09:53:38 +0800

    constants

--- include/ircd.hrl
+++ include/ircd.hrl
@@ -1,3 +1,9 @@
 -record(irc_message, {prefix = false, command, params, trailing = false}).
 -record(irc_user, {username, hostname, servername, realname}).
 -record(irc_agent, {pid, nick, user, host}).
+
+% Max length of user name 
+-define(CLIENT_USER_MAXLEN, 9).
+
+% Max length of a channel name.
+-define(CHANNEL_NAME_MAXLEN, 50).
